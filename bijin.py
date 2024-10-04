d0393ece93984a6fbadd42b788df53bb6d5796f6
bijinalex <bijinalex2028@cs.sjcetpalai.ac.in>
2024-10-04 16:07:58 +0530

    Add files via upload
    basics of python

---
+++ bijin.py
@@ -0,0 +1,67 @@
+'''
+print("hello world")
+print("hello\nworld")
+if 5>2:
+    print("five is greater than two")
+if 5>2:
+    print("five is greater than two")
+if 5>2:
+    print("five is greater than two")
+'''
+'''
+if 5>2:
+   print("five is greater than two")
+   print ("five is greater than two")
+x=5
+x="thomas"
+print (x)
+
+x=str(0)
+y=int(3)
+z=float(6)
+print (x)
+print (y)
+print(z)
+
+
+x=1j
+y="john"
+print(type(x))
+print(type(y))
+a=4
+A="thomas"
+
+x,y,z="orange",'banana',"cherry"
+
+print(x)
+print(y)
+print(z)
+
+x=y=z="orange"
+print(x)
+print(y)
+print(z)
+
+x=" python "
+y=' is '
+z=" awesome "
+print (x+y+z)
+
+username=input("Enter username:")
+print("username is ; " + username)
+
+college=input("enter college:")
+print("my college is: "+ college)
+
+a="hello"
+b=("world")
+c=a+b
+print(c)
+
+a="Hello, World!"
+print(a[10]) 
+
+b="Hello, World!"
+print(b[10:15])
+'''
+
